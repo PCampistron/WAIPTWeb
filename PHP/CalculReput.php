8bee3eb0e686a17e0be8cf77d1e6b6d8ef679660
Pierre Campistron <pier.campistron@gmail.com>
2022-12-13 11:58:09 +0100

    Mise en place de la sélection de critères.

---
+++ PHP/CalculReput.php
@@ -0,0 +1,20 @@
+<?php
+
+    if (isset($_POST['crit1']))
+    {
+        $crit1 = $_POST["crit1"];
+        echo "Critère 1 : " . $crit1;
+    }
+
+    if (isset($_POST['crit2']))
+    {
+        $crit1 = $_POST["crit2"];
+        echo $crit2;
+    }
+
+    if (isset($_POST['crit3']))
+    {
+        $crit1 = $_POST["crit3"];
+        echo $crit3;
+    } 
+?>
---
+++ PHP/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="search.php" method="GET">
+        <input type="text" name="query"/>
+        <input type="text" list=value>
+
+
+        <input type="submit" value="search"/>
+    </form>
+
+    <form action="CalculReput.php" method="POST">
+        <p>Premier critère :</p>
+        <input type="radio" name="crit1" value="dateSortie">
+        <label for="dateSortie">Date de sortie</label><br>
+        <input type="radio" name="crit1" value="nbUtilisateur">
+        <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
+        <input type="radio" name="crit1" value="nbDefi">
+        <label for="nbDefi">Nombre de défis</label>
+
+        <br>  
+
+        <p>Second critère :</p>
+        <input type="radio" name="crit2" value="dateSortie">
+        <label for="dateSortie">Date de sortie</label><br>
+        <input type="radio" name="crit2" value="nbUtilisateur">
+        <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
+        <input type="radio" name="crit2" value="nbDefi">
+        <label for="nbDefi">Nombre de défis</label>
+
+        <br>  
+
+        <p>Troisième critère :</p>
+        <input type="radio" name="crit3" value="dateSortie">
+        <label for="dateSortie">Date de sortie</label><br>
+        <input type="radio" name="crit3" value="nbUtilisateur">
+        <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
+        <input type="radio" name="crit3" value="nbDefi">
+        <label for="nbDefi">Nombre de défis</label>
+
+        <input type="submit" value="Rechercher">
+    </form>
+</body>
+</html>
